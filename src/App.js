a225f19aa8f22507ee25257ec74855796a26d332
Hartrickb <hartrickb@users.noreply.github.com>
2021-11-02 13:27:59 -0400

    layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+// import { TextField, Button, Grid } from '@mui/material';
+import { Button, ChakraProvider, Container, Flex, Grid, GridItem, Heading, Input } from '@chakra-ui/react';
+import { AwesomeButton } from 'react-awesome-button';
+import "react-awesome-button/dist/styles.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ChakraProvider>
+          <Container maxWidth='lg' marginBottom='auto' paddingTop='20px'>
+              <Grid width='full' templateRows='repeat(3, 1fr)' templateColumns='repeat(2, 1fr)' gap={4}>
+                <GridItem colSpan={2}>
+                  <Heading textColor='black'>Lunch Finder</Heading>
+                </GridItem>
+                <GridItem colSpan={2}>
+                  <Input backgroundColor='white' textColor='black' placeholder="Zip Code" size='md'/>
+                </GridItem>
+                <GridItem colSpan={1}>
+                  <Button colorShceme='blue' variant='outline' backgroundColor='gray.100' textColor='black' width='full'>Let me choose</Button>
+                </GridItem>
+                <GridItem colSpan={1}>
+                  <Button colorScheme='blue' width='full'>Pick for me</Button>
+                </GridItem>
+              </Grid>
+          </Container>
+        </ChakraProvider>
+        {/* <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField id="zip-code" label="Zip Code" variant="outlined" />
+          </Grid>
+          <Grid container spacing={12} justifyContent='center'>
+            <Grid item xs={4}>
+              <AwesomeButton type="primary">Pick for me</AwesomeButton>
+            </Grid>
+            <Grid item xs={4}>
+              <AwesomeButton type="instagram">Let me choose</AwesomeButton>
+            </Grid>
+          </Grid>
+        </Grid> */}
       </header>
     </div>
   );
